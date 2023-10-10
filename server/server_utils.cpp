4b76c4a29cd1d217600d23846a871a2e0dc1bf3e
Nikhil Mohite <hello@nikhilmohite.com>
2023-10-10 08:52:35 +0530

    implement hashtable
    Hashing information:
    method: chaining
    resize-method: progressive resizing
    intrusive: yes

--- server/server_utils.cpp
+++ server/server_utils.cpp
@@ -143,6 +143,83 @@ int32_t accept_new_conn(std::vector<Conn *> &fd2conn, int fd)
 static void state_req(Conn *conn);
 static void state_res(Conn *conn);
 
+bool cmd_is(const std::string &word, const char *cmd)
+{
+    return 0 == strcasecmp(word.c_str(), cmd);
+}
+
+static int32_t parse_req(
+    const uint8_t *data, size_t len, std::vector<std::string> &out)
+{
+    if (len < 4)
+    {
+        return -1;
+    }
+    uint32_t n = 0;
+    memcpy(&n, &data[0], 4);
+    if (n > k_max_args)
+    {
+        return -1;
+    }
+
+    size_t pos = 4;
+    while (n--)
+    {
+        if (pos + 4 > len)
+        {
+            return -1;
+        }
+        uint32_t sz = 0;
+        memcpy(&sz, &data[pos], 4);
+        if (pos + 4 + sz > len)
+        {
+            return -1;
+        }
+        out.push_back(std::string((char *)&data[pos + 4], sz));
+        pos += 4 + sz;
+    }
+
+    if (pos != len)
+    {
+        return -1; // trailing garbage
+    }
+    return 0;
+}
+
+int32_t do_request(
+    const uint8_t *req, uint32_t reqlen,
+    uint32_t *rescode, uint8_t *res, uint32_t *reslen)
+{
+    std::vector<std::string> cmd;
+    if (0 != parse_req(req, reqlen, cmd))
+    {
+        msg("bad req");
+        return -1;
+    }
+    if (cmd.size() == 2 && cmd_is(cmd[0], "get"))
+    {
+        *rescode = do_get(cmd, res, reslen);
+    }
+    else if (cmd.size() == 3 && cmd_is(cmd[0], "set"))
+    {
+        *rescode = do_set(cmd, res, reslen);
+    }
+    else if (cmd.size() == 2 && cmd_is(cmd[0], "del"))
+    {
+        *rescode = do_del(cmd, res, reslen);
+    }
+    else
+    {
+        // cmd is not recognized
+        *rescode = RES_ERR;
+        const char *msg = "Unknown cmd";
+        strcpy((char *)res, msg);
+        *reslen = strlen(msg);
+        return 0;
+    }
+    return 0;
+}
+
 bool try_one_request(Conn *conn)
 {
     // try to parse a request from the buffer
@@ -308,14 +385,37 @@ void connection_io(Conn *conn)
     }
 }
 
-uint32_t do_get(
-    const std::vector<std::string> &cmd, uint8_t *res, uint32_t *reslen)
+static bool entry_eq(HNode *lhs, HNode *rhs)
 {
-    if (!g_map.count(cmd[1]))
+    struct Entry *le = container_of(lhs, struct Entry, node);
+    struct Entry *re = container_of(rhs, struct Entry, node);
+    return lhs->hcode == rhs->hcode && le->key == re->key;
+}
+
+uint64_t str_hash(const uint8_t *data, size_t len)
+{
+    uint32_t h = 0x811C9DC5;
+    for (size_t i = 0; i < len; i++)
+    {
+        h = (h + data[i]) * 0x01000193;
+    }
+    return h;
+}
+
+uint32_t do_get(
+    std::vector<std::string> &cmd, uint8_t *res, uint32_t *reslen)
+{
+    Entry key;
+    key.key.swap(cmd[1]);
+    key.node.hcode = str_hash((uint8_t *)key.key.data(), key.key.size());
+
+    HNode *node = hm_lookup(&g_data.db, &key.node, &entry_eq);
+    if (!node)
     {
         return RES_NX;
     }
-    std::string &val = g_map[cmd[1]];
+
+    const std::string &val = container_of(node, Entry, node)->val;
     assert(val.size() <= k_max_msg);
     memcpy(res, val.data(), val.size());
     *reslen = (uint32_t)val.size();
@@ -323,96 +423,45 @@ uint32_t do_get(
 }
 
 uint32_t do_set(
-    const std::vector<std::string> &cmd, uint8_t *res, uint32_t *reslen)
+    std::vector<std::string> &cmd, uint8_t *res, uint32_t *reslen)
 {
     (void)res;
     (void)reslen;
-    g_map[cmd[1]] = cmd[2];
+
+    Entry key;
+    key.key.swap(cmd[1]);
+    key.node.hcode = str_hash((uint8_t *)key.key.data(), key.key.size());
+
+    HNode *node = hm_lookup(&g_data.db, &key.node, &entry_eq);
+    if (node)
+    {
+        container_of(node, Entry, node)->val.swap(cmd[2]);
+    }
+    else
+    {
+        Entry *ent = new Entry();
+        ent->key.swap(key.key);
+        ent->node.hcode = key.node.hcode;
+        ent->val.swap(cmd[2]);
+        hm_insert(&g_data.db, &ent->node);
+    }
     return RES_OK;
 }
 
 uint32_t do_del(
-    const std::vector<std::string> &cmd, uint8_t *res, uint32_t *reslen)
+    std::vector<std::string> &cmd, uint8_t *res, uint32_t *reslen)
 {
     (void)res;
     (void)reslen;
-    g_map.erase(cmd[1]);
+
+    Entry key;
+    key.key.swap(cmd[1]);
+    key.node.hcode = str_hash((uint8_t *)key.key.data(), key.key.size());
+
+    HNode *node = hm_pop(&g_data.db, &key.node, &entry_eq);
+    if (node)
+    {
+        delete container_of(node, Entry, node);
+    }
     return RES_OK;
-}
-
-bool cmd_is(const std::string &word, const char *cmd)
-{
-    return 0 == strcasecmp(word.c_str(), cmd);
-}
-
-static int32_t parse_req(
-    const uint8_t *data, size_t len, std::vector<std::string> &out)
-{
-    if (len < 4)
-    {
-        return -1;
-    }
-    uint32_t n = 0;
-    memcpy(&n, &data[0], 4);
-    if (n > k_max_args)
-    {
-        return -1;
-    }
-
-    size_t pos = 4;
-    while (n--)
-    {
-        if (pos + 4 > len)
-        {
-            return -1;
-        }
-        uint32_t sz = 0;
-        memcpy(&sz, &data[pos], 4);
-        if (pos + 4 + sz > len)
-        {
-            return -1;
-        }
-        out.push_back(std::string((char *)&data[pos + 4], sz));
-        pos += 4 + sz;
-    }
-
-    if (pos != len)
-    {
-        return -1; // trailing garbage
-    }
-    return 0;
-}
-
-int32_t do_request(
-    const uint8_t *req, uint32_t reqlen,
-    uint32_t *rescode, uint8_t *res, uint32_t *reslen)
-{
-    std::vector<std::string> cmd;
-    if (0 != parse_req(req, reqlen, cmd))
-    {
-        msg("bad req");
-        return -1;
-    }
-    if (cmd.size() == 2 && cmd_is(cmd[0], "get"))
-    {
-        *rescode = do_get(cmd, res, reslen);
-    }
-    else if (cmd.size() == 3 && cmd_is(cmd[0], "set"))
-    {
-        *rescode = do_set(cmd, res, reslen);
-    }
-    else if (cmd.size() == 2 && cmd_is(cmd[0], "del"))
-    {
-        *rescode = do_del(cmd, res, reslen);
-    }
-    else
-    {
-        // cmd is not recognized
-        *rescode = RES_ERR;
-        const char *msg = "Unknown cmd";
-        strcpy((char *)res, msg);
-        *reslen = strlen(msg);
-        return 0;
-    }
-    return 0;
 }
